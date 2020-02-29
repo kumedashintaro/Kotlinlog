17265cdb811af7291b1567d14b5ffa6852e34a14
kumedashintarou <pii.k2.bys@gmail.com>
2020-02-29 20:54:29 +0900

    最初のコミット

---
+++ app/src/main/java/com/example/kotlinlog/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.kotlinlog
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val human = Human("ポチ", 3,"将来")
+
+        human.say()
+
+        human.think()
+
+
+    }
+}
